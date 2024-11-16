d86eccc76af4b9f3f19f63079a8ebf493a26c657
chovYena <chovYena@users.noreply.github.com>
2024-11-16 12:04:05 +0000

    deploy: c68f651bd7cbece774a8cca7fd5e4ecb2f4e80ef

---
+++ assets/hoisted.DzRIyBgz.js
@@ -0,0 +1,1 @@
+import"./hoisted.BuKvBNNg.js";import"./module.esm.D7K9r6F4.js";document.querySelector('a[href="#latest-posts"]')?.addEventListener("click",t=>{t.preventDefault(),document.querySelector("#latest-posts")?.scrollIntoView({behavior:"smooth"})});
---
+++ assets/page.DfepQkps.js
@@ -0,0 +1,1 @@
+import{m}from"./module.esm.D7K9r6F4.js";window.Alpine=m;m.start();
